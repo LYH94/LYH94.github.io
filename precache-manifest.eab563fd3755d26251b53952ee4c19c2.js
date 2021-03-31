6e6bbf182c2dbdaa005f7cfe796b18172e7dff78
LYH94 <49682614+LYH94@users.noreply.github.com>
2021-03-31 16:21:14 +0000

    Deploying to gh-pages from master e36bda587b5c08fb23acd4981b83922171db418e

---
+++ precache-manifest.eab563fd3755d26251b53952ee4c19c2.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2570fc0a0d90871016df",
+    "url": "css/admin-products.dd99721b.css"
+  },
+  {
+    "revision": "0714de24165e5f4a4eb7",
+    "url": "css/app.012c354e.css"
+  },
+  {
+    "revision": "d2e5244df6e162639545",
+    "url": "css/categories.96bc9d19.css"
+  },
+  {
+    "revision": "8170aa89ff22fafc102c",
+    "url": "css/chunk-vendors.eebf76f3.css"
+  },
+  {
+    "revision": "2e25ca7dc0cb90c8a8cf1c446eb32656",
+    "url": "img/photo.2e25ca7d.png"
+  },
+  {
+    "revision": "d277092e6ae902f4635094996f4a1efa",
+    "url": "index.html"
+  },
+  {
+    "revision": "50f12a9e2706221f168c",
+    "url": "js/about.b9b1290f.js"
+  },
+  {
+    "revision": "2570fc0a0d90871016df",
+    "url": "js/admin-products.f510ec11.js"
+  },
+  {
+    "revision": "0714de24165e5f4a4eb7",
+    "url": "js/app.2c7ed179.js"
+  },
+  {
+    "revision": "d2e5244df6e162639545",
+    "url": "js/categories.04cc4a7a.js"
+  },
+  {
+    "revision": "8170aa89ff22fafc102c",
+    "url": "js/chunk-vendors.b23789b1.js"
+  },
+  {
+    "revision": "8b044e052a331a78299e983f8f0fb6be",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "1476454339b504d4ed8ab2209ffbbea2",
+    "url": "portfolio/portfolio-01.png"
+  },
+  {
+    "revision": "557c9a0573897471fe16ee98fc7de3d2",
+    "url": "portfolio/portfolio-02.jpg"
+  },
+  {
+    "revision": "6198be5e045289268138c41faa965b1b",
+    "url": "portfolio/portfolio-03.jpg"
+  },
+  {
+    "revision": "a9cd644a8afacb92b8cf2ecd4c499d79",
+    "url": "portfolio/portfolio-04.jpg"
+  },
+  {
+    "revision": "184e1eb2fef35dc4c8989cd44fdd5772",
+    "url": "portfolio/portfolio-05.jpg"
+  },
+  {
+    "revision": "3a65d2a33b91426033dc96fe13a8c6cf",
+    "url": "portfolio/portfolio-06.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
